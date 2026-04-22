5cb3639f1676917c0438c53f0db9201cf1353304
Paul Ramsey <22893884+paulramsey@users.noreply.github.com>
2026-04-22 13:14:46 -0500

    fix: Add missing extension and pin HNSW.

--- data/alloydb-extensions.sql
+++ data/alloydb-extensions.sql
@@ -5,5 +5,6 @@ CREATE EXTENSION IF NOT EXISTS bigquery_fdw;
 CREATE EXTENSION IF NOT EXISTS rum;
 CREATE EXTENSION IF NOT EXISTS alloydb_scann;
 CREATE EXTENSION IF NOT EXISTS g_distributed_exec;
+CREATE EXTENSION IF NOT EXISTS pg_hint_plan;
 
 ALTER DATABASE postgres SET google_ml_integration.enable_preview_ai_functions = on;
